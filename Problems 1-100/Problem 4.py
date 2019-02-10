0229fe2b74b7bb1fc508de9ad665bc329915ddfd
abfs937 <charlottefan1992@gmail.com>
2019-02-10 14:03:18 +0000

    Problems 4

--- Problems 1-100/Problem 4.py
+++ Problems 1-100/Problem 4.py
@@ -13,9 +13,9 @@ for a in list1:
 
 for i in list3:
     d = list(str(i))
-    if d[0] == d[-1] and d[1] == d[-2] and d[2] == d[-3] and d[0] == '9':
+    if d[::-1] == d and d[0] == '9':
         e = "".join(d)
-print(e)
+print('The largest palindrome made from the product of two 3-digit numbers is:', e)
 
 
 
